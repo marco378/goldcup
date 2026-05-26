ec41b7ecfec2f9b83989dfd45eec1f4b31993f4e
Aryan Tavish <aryantavish2003@gmail.com>
2026-05-26 12:35:21 +0530

    updated today's live links 26th may

--- data/liveMatches.ts
+++ data/liveMatches.ts
@@ -1,4 +1,4 @@
 export const LIVE_MATCH_STREAMS: Record<string, string> = {
-  'group-a-2026-05-26-1': 'https://www.youtube.com/live/AL_JlX6ZAQQ?si=JgDSKn2suLaDwTs6',
-  'group-a-2026-05-26-2': 'https://www.youtube.com/live/3ZSKYBUwIqI?si=2iYkVR5DfQnA-gjQ',
+  'group-b-2026-05-26-1': 'https://www.youtube.com/live/AL_JlX6ZAQQ?si=JgDSKn2suLaDwTs6',
+  'group-b-2026-05-26-2': 'https://www.youtube.com/live/3ZSKYBUwIqI?si=2iYkVR5DfQnA-gjQ',
 }
