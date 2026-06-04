1f592138ad2e308863aa348df9af6583f69e8804
Aryan Tavish <aryantavish2003@gmail.com>
2026-06-04 15:15:17 +0530

    added link and live matches data 4th

--- data/liveMatches.ts
+++ data/liveMatches.ts
@@ -1,4 +1,4 @@
 export const LIVE_MATCH_STREAMS: Record<string, string> = {
-  'qf-2026-06-03-1': 'https://www.youtube.com/live/ZcWSj7aX7TA?si=ZP1cjThtFnLRNjSG',
-  'qf-2026-06-03-2': 'https://www.youtube.com/live/aK5j918Deeo?si=4CADTOkQPdb8VMOJ',
+  'qf-2026-06-04-1': 'https://www.youtube.com/live/jSbILQjwdTI?si=-g-JYEesRgufvCdl',
+  'qf-2026-06-04-2': 'https://www.youtube.com/live/aK5j918Deeo?si=4CADTOkQPdb8VMOJ',
 }
