c42cd1408ee52bbdf26fa0605e5f05dc06f385d0
Aryan Tavish <aryantavish2003@gmail.com>
2026-06-05 15:42:10 +0530

    updated SF link

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import localFont from 'next/font/local'
 import { Manrope } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 
 const coluna = localFont({
   src: './fonts/Coluna.otf',
@@ -34,7 +33,6 @@ export default function RootLayout({
       <body className={`${coluna.variable} ${manrope.variable} is-loading`}>
         <div className="globalGlow" aria-hidden="true" />
         {children}
-        <Analytics />
       </body>
     </html>
   )
--- data/liveMatches.ts
+++ data/liveMatches.ts
@@ -1,4 +1,4 @@
 export const LIVE_MATCH_STREAMS: Record<string, string> = {
-  'qf-2026-06-04-1': 'https://www.youtube.com/live/jSbILQjwdTI?si=-g-JYEesRgufvCdl',
-  'qf-2026-06-04-2': 'https://www.youtube.com/live/aK5j918Deeo?si=4CADTOkQPdb8VMOJ',
+  'sf-2026-06-05-1': 'https://www.youtube.com/live/tteopAhxKSk?si=PvvOkdOEOAaLFHLz',
+
 }
