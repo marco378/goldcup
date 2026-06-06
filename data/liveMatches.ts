1296b8954dd35d4feddaef71631c898456273542
Aryan Tavish <aryantavish2003@gmail.com>
2026-06-06 13:36:19 +0530

    updated SF-3 link

--- data/liveMatches.ts
+++ data/liveMatches.ts
@@ -1,4 +1,4 @@
 export const LIVE_MATCH_STREAMS: Record<string, string> = {
-  'sf-2026-06-05-1': 'https://www.youtube.com/live/tteopAhxKSk?si=PvvOkdOEOAaLFHLz',
+  'sf-2026-06-06-2': 'https://www.youtube.com/live/49IpcigCY4s?si=p1s_PLyUoE1w7iaf',
 
 }
